a05612f3b4979b73af5dc5bb3e157a1ee027b3d5
root <root@ip-172-31-18-151.us-east-2.compute.internal>
2020-06-04 04:55:12 +0000

    Dbutil.java

--- Dbutil.java
+++ Dbutil.java
@@ -1,8 +1,18 @@
+this is to test reversion
+
+
 dgjfgfsjhgcjhGHCU VH
 FHCJASDKJCGS
+<<<<<<< HEAD
 
 to test 
 
 
 
 updatedhckjhakjjakjsfdkj
+=======
+hckjhakjjakjsfdkj
+
+
+
+>>>>>>> parent of d0491a4... testone
